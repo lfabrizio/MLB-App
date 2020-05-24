2945fb51d9dc22eb45b5cc44a4b5e15ae7885fd5
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-24 19:17:00 -0400

    counter

---
+++ mlb-app/src/Components/Counter/Counter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// function component
+export default function ThirdComponent(){
+    return (
+      <div className="thirdComponent">
+        ThirdComponent
+      </div>
+    );
+  }
