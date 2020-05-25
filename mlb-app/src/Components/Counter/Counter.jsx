c7b7ba20147efd9a5cc62130635c8a5b62f49d6a
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-25 12:01:22 -0400

    handling click event

--- mlb-app/src/Components/Counter/Counter.jsx
+++ mlb-app/src/Components/Counter/Counter.jsx
@@ -5,10 +5,14 @@ import './Counter.css'
  function Counter(){
     return (
       <div className="counter">
-        <button>+1</button>
+        <button onClick={increment}>+1</button>
         <span className="count" >0</span>
       </div>
     );
   }
+
+function increment() {
+    console.log('increment');
+}
 
   export default Counter
