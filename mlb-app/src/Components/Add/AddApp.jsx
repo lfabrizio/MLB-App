2c7848d271589a828bfe1a92e24443038f726318
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-26 18:01:45 -0400

    state

--- mlb-app/src/Components/Add/AddApp.jsx
+++ mlb-app/src/Components/Add/AddApp.jsx
@@ -11,11 +11,31 @@ class AddApp extends Component {
 }
 
 class LoginComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: 'lf',
+            password: ''
+        }
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    }
+
+    handleUsernameChange(event){
+        console.log(event.target.value);
+        this.setState({username:event.target.value} )
+    }
+
+    handlePasswordChange(event){
+        console.log({password:event.target.value})
+    }
+
+
     render() {
         return (
             <div>
-        User Name: <input type="text" name="username"/>
-        Password: <input type="password" name="password"/>
+        User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange}/>
+        Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
         <button>Login</button>
         </div>
         )
