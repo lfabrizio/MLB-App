d35eb52767b3b5b466fb044de4fd1461ac56cfd7
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-31 19:05:21 -0400

    import cross origins

--- mlb-app/src/API/Todo/HelloWorldService.js
+++ mlb-app/src/API/Todo/HelloWorldService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-
+//import cross origins from server
 class HelloWorldService {
     executeHelloWorld() {
         return axios.get('http://localhost:8080/hello-world');
