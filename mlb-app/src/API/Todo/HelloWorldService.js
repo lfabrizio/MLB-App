e0a0a303e9f0ef1f335c5ded5ce6483b33f21b16
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-06-01 19:33:48 -0400

    add data

--- mlb-app/src/API/Todo/HelloWorldService.js
+++ mlb-app/src/API/Todo/HelloWorldService.js
@@ -2,11 +2,18 @@ import axios from 'axios'
 
 //import cross origins from server
 class HelloWorldService {
+
     executeHelloWorld() {
         return axios.get('http://localhost:8080/hello-world');
         
     }
-
 }
+    // class HelloWorldBeanService {
+    //     executeHelloWorld() {
+    //         return axios.get('http://localhost:8080/hello-world-bean');
+            
+    //     }
+
+// }
 
 export default new HelloWorldService()
--- mlb-app/src/Components/Learning-Examples/FirstComponent.jsx
+++ mlb-app/src/Components/Learning-Examples/FirstComponent.jsx
@@ -1,37 +1,40 @@
 import React, { Component } from 'react';
 import HelloWorldService from '../../API/Todo/HelloWorldService'
 
-export default class FirstComponent extends Component {
+export default class WelcomeComponent extends Component {
   constructor(props) {
     super(props)
     this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
-    this.state = {
-      welcomeMessage : ''
-    }
-    this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+ 
   }
     render() {
     return (
       <>
       <div className="firstComponent">
-        Get customized welcome message
-        <button onClick={this.retrieveWelcomeMessage}> Press Here</button>
+        
+        
       </div>
-      
-       <div> {this.state.welcomeMessage} </div>
+      <div>
+      <button onClick={this.retrieveWelcomeMessage}> Press Here</button>
+        </div>
       
       </>
     )
   }
     retrieveWelcomeMessage() {
-      HelloWorldService.executeHelloWorld()
-      .then(response => this.handleSuccessfulResponse(response) )
+       HelloWorldService.executeHelloWorld()
+       .then(response => this.handleSuccessfulResponse(response) )
       //.catch()
-    }
+
+    //   HelloWorldService.executeHelloWorldBeanService()
+    //   .then(response => this.handleSuccessfulResponse(response) )
+    //   //.catch()
+     }
 
     handleSuccessfulResponse(response) {
-      this.setState({ welcomeMessage : response.data})
+      console.log(response)
+      this.setState({ welcomeMessage : response.data.message})
      
     }
-    
-  }
+    }
+  
