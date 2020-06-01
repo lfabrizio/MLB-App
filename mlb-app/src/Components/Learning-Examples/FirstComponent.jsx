cc8d17dbb3871f360f573b51e3d5e1adf69d8830
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-31 21:16:53 -0400

    allow cross origins

--- mlb-app/src/Components/Learning-Examples/FirstComponent.jsx
+++ mlb-app/src/Components/Learning-Examples/FirstComponent.jsx
@@ -5,19 +5,33 @@ export default class FirstComponent extends Component {
   constructor(props) {
     super(props)
     this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+    this.state = {
+      welcomeMessage : ''
+    }
+    this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
   }
     render() {
     return (
+      <>
       <div className="firstComponent">
         Get customized welcome message
         <button onClick={this.retrieveWelcomeMessage}> Press Here</button>
       </div>
-    );
+      
+       <div> {this.state.welcomeMessage} </div>
+      
+      </>
+    )
   }
     retrieveWelcomeMessage() {
       HelloWorldService.executeHelloWorld()
-      .then(response => console.log(response) )
+      .then(response => this.handleSuccessfulResponse(response) )
       //.catch()
+    }
+
+    handleSuccessfulResponse(response) {
+      this.setState({ welcomeMessage : response.data})
+     
     }
     
   }
