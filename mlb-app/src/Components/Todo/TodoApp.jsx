e82101f830c9d0301a8b3606aca65da08923e06a
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-06-02 19:05:47 -0400

    simplify login credentials

--- mlb-app/src/Components/Todo/TodoApp.jsx
+++ mlb-app/src/Components/Todo/TodoApp.jsx
@@ -85,8 +85,10 @@ class LoginComponent extends Component {
     render() {
         return (
             <div>
-                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
-                <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>
+                {/* <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/> */}
+                {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
+                {this.state.showSuccessMessage && <div>Successful Login!</div>}
+                {/* <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/> */}
             User Name : <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
             Password : <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
             <button onClick={this.loginClicked}>Login</button>
