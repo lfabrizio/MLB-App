795c4ebeed23e02e26c461ff1af53869d4a1d147
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-06-08 20:09:26 -0400

    add to welcome component

--- mlb-app/src/Components/Todo/WelcomeComponent.jsx
+++ mlb-app/src/Components/Todo/WelcomeComponent.jsx
@@ -3,9 +3,20 @@ import {BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 class WelcomeComponent extends Component {
     render() {
-        return <div>
-            Welcome {this.props.match.params.name}! You can manage your todos <Link to="/todos">here</Link>
+        return (
+            <>
+            <h1>Welcome</h1>
+         <div className="container">
+            Welcome {this.props.match.params.name}
+             You can manage your todos <Link to="/todos">here</Link>
             </div>
+            <div className="container">
+                click for customized message
+                manage todos <Link to="/todos">here</Link>
+                <button onClick={this.retrieveWelcomeMessage}>Get Message</button>
+            </div>
+            </>
+        )
     }
 }
 
