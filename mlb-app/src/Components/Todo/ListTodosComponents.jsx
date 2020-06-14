88fd6e81237fb09632e71c4fade26cbcc4d6efa4
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-06-14 18:56:07 -0400

    12

--- mlb-app/src/Components/Todo/ListTodosComponents.jsx
+++ mlb-app/src/Components/Todo/ListTodosComponents.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import TodoDataService from '../../API/Todo/TodoDataService.js'
+import AuthenticationService from './AuthenticationService.js'
 
 class ListTodosComponent extends Component {
     constructor(props){
@@ -7,11 +8,19 @@ class ListTodosComponent extends Component {
         this.state = {
             todos: 
             [
-                {id: 1, description: 'Learn React', done:false, targetDate: new Date()},
-                {id: 2, description: 'Become an Expert at React', done:false, targetDate: new Date()},
-                {id: 3, description: 'Travel', done:false, targetDate: new Date()}
             ]
         }
+    }
+
+    componentDidMount() {
+        let username = AuthenticationService.getLoggedInUserName();
+        TodoDataService.retrieveAllTodos(username)
+        .then(
+            response => {
+                // console.log(response)
+                this.setState({todos : response.data})
+            }
+        )
     }
     render() {
         return <div>
