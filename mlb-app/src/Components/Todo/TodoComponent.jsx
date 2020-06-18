46792af4559f0fdc9808989def9a46e9727abdc1
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-06-17 21:24:33 -0400

    18a

--- mlb-app/src/Components/Todo/TodoComponent.jsx
+++ mlb-app/src/Components/Todo/TodoComponent.jsx
@@ -1,6 +1,14 @@
 import React, {Component} from 'react'
 
 class TodoComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            id : 1,
+            desription : 'Learn forms',
+            targetDate : new Date()
+        }
+    }
     render() {
         return <div>
             Todo Component for id - {this.props.match.params.id} </div>
