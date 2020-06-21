0731d1ecc812e967ceedd306240dac490abddba0
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-06-21 10:48:47 -0400

    25pta

--- mlb-app/src/API/Todo/TodoDataService.js
+++ mlb-app/src/API/Todo/TodoDataService.js
@@ -13,10 +13,17 @@ class TodoDataService {
     }
 
 
+    updateTodo(name, id, todo) {
+        //console.log('executed service')
+        return axios.put(`http://localhost:8080/users/${name}/todos/${id}`, todo); 
+    }  
+    
     deleteTodo(name, id) {
         //console.log('executed service')
         return axios.get(`http://localhost:8080/users/${name}/todos/${id}`); 
     }
+
+
 }
 
 export default new TodoDataService()
