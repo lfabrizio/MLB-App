44a3ef4d3d8412d8049f722dd9e705d36e980dd6
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-07-16 17:22:25 -0400

    use constants for urls

--- mlb-app/src/Components/Todo/AuthenticationService.js
+++ mlb-app/src/Components/Todo/AuthenticationService.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
+
 class AuthenticationService {
 
     executeBasicAuthenticationService(username, password){
@@ -21,12 +23,12 @@ class AuthenticationService {
     registerSuccessfulLogin(username, password){
         // let basicAuthHeader = 'Basic' + window.btoa(username + ":" + password)
     // console.log('registered');
-        sessionStorage.setItem('authenticatedUser', username);
+        sessionStorage.setItem('USER_NAME_SESSION_ATTRIBUTE_NAME', username);
         this.setupAxiosInterceptors(this.createBasicAuthToken(username, password))
     }
 
     registerSuccessfulLoginForJwt(username, token){
-        sessionStorage.setItem('authenticatedUser', username)
+        sessionStorage.setItem('USER_NAME_SESSION_ATTRIBUTE_NAME', username)
         this.setupAxiosInterceptors(this.createJWTToken(token))
     }
 
@@ -35,17 +37,17 @@ class AuthenticationService {
     }
 
     logout() {
-        sessionStorage.removeItem('autheticatedUser');
+        sessionStorage.removeItem('USER_NAME_SESSION_ATTRIBUTE_NAME');
     }
 
     isUserLoggedIn() {
-        let user = sessionStorage.getItem('authenticatedUser')
+        let user = sessionStorage.getItem('USER_NAME_SESSION_ATTRIBUTE_NAME')
         if(user===null) return false
         return true
     }
 
     getLoggedInUserName() {
-        let user = sessionStorage.getItem('authenticatedUser')
+        let user = sessionStorage.getItem('USER_NAME_SESSION_ATTRIBUTE_NAME')
         if(user===null) return ''
         return user
     }
