bcc083fe53a428e4120cdc1081f8b596bb974e88
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-07-17 20:49:50 -0400

    comment

--- mlb-app/src/API/Todo/TodoDataService.js
+++ mlb-app/src/API/Todo/TodoDataService.js
@@ -1,31 +1,32 @@
 import axios from 'axios'
+import { API_URL } from '../../Constants'
 
 class TodoDataService {
     retrieveAllTodos(name) {
-        console.log('executed service')
-        return axios.get(`http://localhost:8080/users/${name}/todos`); 
+        // console.log('executed service')
+        return axios.get(`${API_URL}/users/${name}/todos`); 
     }
 
 
     retrieveTodo(name, id) {
-        console.log('executed service')
-        return axios.get(`http://localhost:8080/users/${name}/todos/${id}`); 
+        // console.log('executed service')
+        return axios.get(`${API_URL}/users/${name}/todos/${id}`); 
     }
 
 
     updateTodo(name, id, todo) {
         //console.log('executed service')
-        return axios.put(`http://localhost:8080/users/${name}/todos/${id}`, todo); 
+        return axios.put(`${API_URL}/users/${name}/todos/${id}`, todo); 
     }  
     
     deleteTodo(name, id) {
         //console.log('executed service')
-        return axios.get(`http://localhost:8080/users/${name}/todos/${id}`); 
+        return axios.get(`${API_URL}/users/${name}/todos/${id}`); 
     }
 
     createTodo(name, todo) {
         //console.log('executed service')
-        return axios.post(`http://localhost:8080/users/${name}/todos/`, todo); 
+        return axios.post(`${API_URL}/users/${name}/todos/`, todo); 
     }
 
 }
