c5a0679494e6858625d4e956c15d62e525ef7585
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-07-28 18:25:58 -0400

    stats

--- mlb-app/src/Components/Todo/WelcomeComponent.jsx
+++ mlb-app/src/Components/Todo/WelcomeComponent.jsx
@@ -33,7 +33,7 @@ import '../Learning-Examples/Title.css'
     <li><a href="https://github.com/lfabrizio/MLB-App"><span data-text="News">News</span></a></li>
     <li><a href="https://www.mlb.com/scores"><span data-text="Scores">Scores</span></a></li>
     <li><a href="https://www.mlb.com/standings"><span data-text="Standings">Standings</span></a></li>
-    <li><a href="https://www.baseball-reference.com/"><span data-text="Statistics">Statistics</span></a></li>
+    <li><a href="https://www.mlb.com/stats"><span data-text="Statistics">Statistics</span></a></li>
     <li><a href="https://www.cbssports.com/fantasy/baseball/rankings/"><span data-text="Fantasy">Fantasy Baseball</span></a></li>
   </ul>
 </header>
