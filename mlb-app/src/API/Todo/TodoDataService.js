4db9024c1ee307eb1eb7a2f52bb044e791b9b139
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-03 17:16:20 -0400

    update data service

--- mlb-app/src/API/Todo/TodoDataService.js
+++ mlb-app/src/API/Todo/TodoDataService.js
@@ -1,32 +1,32 @@
 import axios from 'axios'
-import { API_URL } from '../../Constants'
+import { API_URL, JPA_API_URL } from '../../Constants'
 
 class TodoDataService {
     retrieveAllTodos(name) {
         // console.log('executed service')
-        return axios.get(`${API_URL}/users/${name}/todos`); 
+        return axios.get(`${JPA_API_URL}/users/${name}/todos`); 
     }
 
 
     retrieveTodo(name, id) {
         // console.log('executed service')
-        return axios.get(`${API_URL}/users/${name}/todos/${id}`); 
+        return axios.get(`${JPA_API_URL}/users/${name}/todos/${id}`); 
     }
 
 
     updateTodo(name, id, todo) {
         //console.log('executed service')
-        return axios.put(`${API_URL}/users/${name}/todos/${id}`, todo); 
+        return axios.put(`${JPA_API_URL}/users/${name}/todos/${id}`, todo); 
     }  
     
     deleteTodo(name, id) {
         //console.log('executed service')
-        return axios.get(`${API_URL}/users/${name}/todos/${id}`); 
+        return axios.get(`${JPA_API_URL}/users/${name}/todos/${id}`); 
     }
 
     createTodo(name, todo) {
         //console.log('executed service')
-        return axios.post(`${API_URL}/users/${name}/todos/`, todo); 
+        return axios.post(`${JPA_API_URL}/users/${name}/todos/`, todo); 
     }
 
 }
