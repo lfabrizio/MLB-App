34c40f1b1270f31c873ac72c95122143e4a843a0
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-08 11:01:31 -0400

    update

--- mlb-app/src/Components/Todo/WelcomeComponent.jsx
+++ mlb-app/src/Components/Todo/WelcomeComponent.jsx
@@ -20,7 +20,7 @@ import '../Learning-Examples/Title.css'
 
 <div className="breaking_news">
   <div className="label ripple">Breaking News</div>
-  <div className="news_title">MLB and MLBPA agree to seven inning Doubleheaders</div>
+  <div className="news_title">Miami Marlins best record in through first two weeks</div>
   
 </div>
             
