d94be10c9c6e7a3ce3227ffd4c2c566245edcf46
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-12 17:59:10 -0400

    fantasy pts

---
+++ mlb-app/src/Components/Todo/FantasyComponent.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+// start component to display fantasy baseball rankings 2020
+// maybe projections, and fantasy points
+
+
+
+
+
+export default FantasyComponent
